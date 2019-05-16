21fc7adef6dc89099aad8e43964519c47c5eac0a
rawazkader85 <rkade103@uottawa.ca>
2019-05-16 09:29:18 -0400

    First working prototype for UI of Midicsv tool.

---
+++ Logic/Converter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Midi_Analyzer.Logic
+{
+    class Converter
+    {
+        public bool RunCSVBatchFile(string[] sourceFiles, string dest)
+        {
+            try
+            {
+                string fileName = "CsvGenerator.bat";
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    foreach (string file in sourceFiles)
+                    {
+                        string[] fileSplit = file.Split('\\');
+                        string sFileName = fileSplit[fileSplit.Length - 1].Split('.')[0];
+                        writer.WriteLine("Midicsv \"" + file + "\" \"" + dest +"\\"+ sFileName + ".csv\"");
+                    }
+                    writer.WriteLine("EXIT /B");
+                }
+                Process process = Process.Start(fileName);
+                process.WaitForExit();
+                File.Delete(fileName);
+                Process.Start(@"" + dest);
+                return true;
+            }
+            catch(Exception e){
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+        }
+        public bool RunMIDIBatchFile(string[] sourceFiles, string dest)
+        {
+            try
+            {
+                string fileName = "MIDIGenerator.bat";
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    foreach (string file in sourceFiles)
+                    {
+                        string[] fileSplit = file.Split('\\');
+                        string sFileName = fileSplit[fileSplit.Length - 1].Split('.')[0];
+                        writer.WriteLine("Csvmidi \"" + file + "\" \"" + dest + "\\" + sFileName + ".mid\"");
+                    }
+                    writer.WriteLine("EXIT /B");
+                }
+                Process process = Process.Start(fileName);
+                process.WaitForExit();
+                File.Delete(fileName);
+                Process.Start(@"" + dest);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+        }
+
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Midi_Analyzer.Logic;
+
+namespace Midi_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string sourceFileType;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            sourceFileType = "MIDI";
+        }
+
+        private void buttonCheckChange(object sender, RoutedEventArgs e)
+        {
+            /*
+             * This method is meant to clear the contents of the source path and array should the user pick a different file type.
+             * It also checks which radio button is now checked, and assigns that to the sourceFileType variable.
+             * */
+            TextBox path = (TextBox)(((FrameworkElement)sender).Parent as FrameworkElement).FindName("sourcePath");
+            path.Text = "";
+            RadioButton midiButton = (RadioButton)(((FrameworkElement)sender).Parent as FrameworkElement).FindName("midiButton");
+            if(midiButton.IsChecked == true)
+            {
+                sourceFileType = "MIDI";
+            }
+            else
+            {
+                sourceFileType = "CSV";
+            }
+            Console.WriteLine(sourceFileType);
+        }
+
+        private void BrowseForFile(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Multiselect = true;
+            if(sourceFileType == "MIDI")
+            {
+                dlg.Filter = "MIDI files|*.MID;*.MIDI";
+            }
+            else
+            {
+                dlg.DefaultExt = ".csv";
+                dlg.Filter = "CSV Files (*.csv)|*.csv";
+            }
+            Nullable<bool> result = dlg.ShowDialog();
+            if(result == true && dlg.FileNames.Length != 0)
+            {
+                TextBox path = (TextBox)(((FrameworkElement)sender).Parent as FrameworkElement).FindName("sourcePath");
+                path.Text = String.Join(";\n", dlg.FileNames);
+            }
+        }
+        private void BrowseForFolder(object sender, RoutedEventArgs e)
+        {
+            var dlg = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dlg.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBox path = (TextBox)(((FrameworkElement)sender).Parent as FrameworkElement).FindName("destinationPath");
+                path.Text = dlg.SelectedPath;
+            }
+        }
+        private void ConvertFile(object sender, RoutedEventArgs e)
+        {
+            TextBox sPath = (TextBox)(((FrameworkElement)sender).Parent as FrameworkElement).FindName("sourcePath");
+            TextBox destPath = (TextBox)(((FrameworkElement)sender).Parent as FrameworkElement).FindName("destinationPath");
+            string[] sourceFiles = sPath.Text.Replace("\n", "").Split(';');
+            string destinationFolder = destPath.Text;
+            Converter converter = new Converter();
+            if(sourceFileType == "CSV")
+            {
+                Console.WriteLine("Running conversion to MIDI...");
+                converter.RunMIDIBatchFile(sourceFiles, destinationFolder);
+            }
+            else if(sourceFileType == "MIDI")
+            {
+                Console.WriteLine("Running conversion to CSV...");
+                converter.RunCSVBatchFile(sourceFiles, destinationFolder);
+            }
+            else
+            {
+                Console.WriteLine("There was an error with the source file type selection.");
+            }
+        }
+    }
+}
